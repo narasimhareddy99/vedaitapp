94d9a1200d0a4c3e1b43c29861325d44422267a1
Narasimha <gunda.narasimha.reddy93@gmail.com>
2024-02-06 15:20:28 +0530

    added a python file

--- main.py
+++
@@ -1,3 +0,0 @@
-num=int(input("enter the number: "))
-for i in range(1,11):
-    print(num,"*",i,"=",num*i)
---
+++ tables.py
@@ -0,0 +1,6 @@
+count=0
+num=int(input("enter the value"))
+while count<=10:           
+           prod=num*count
+           print(num,"*",count,"=",prod)
+           cout=count+1
